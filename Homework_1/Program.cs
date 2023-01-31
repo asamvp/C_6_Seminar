f8135ddcfbc0266aa7d5f38938eed3e76c30f37e
asam <asamvp@mail.ru>
2023-01-31 11:08:19 +0500

    Добавил ДЗ

---
+++ Homework_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int InputInt(string message) //ввод числа
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+int[] InputMassive(int M) // формирование массива введенных чисел
+{
+    int[] massive = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        massive[i] = InputInt($"Введите число {i + 1} -> ");
+    }
+    return massive;
+}
+int Plus(int[] massive) //Количество чисел в массиве больше 0
+{
+    int summplus = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] > 0) summplus++;
+    }
+    return summplus;
+}
+Console.Clear();
+int M = InputInt("Сколько чисел вы хотите ввести? > ");
+System.Console.WriteLine($"Количество чисел в массиве больше 0 = {Plus(InputMassive(M))}");
---
+++ Homework_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1,      y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+int InputInt(string message) //ввод числа
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+Console.Clear();
+double b1 = InputInt("Введите b1 > ");
+double k1 = InputInt("Введите k1 > ");
+double b2 = InputInt("Введите b2 > ");
+double k2 = InputInt("Введите k2 > ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+System.Console.WriteLine($"Точка пересечения координат = ({x}; {y})");
